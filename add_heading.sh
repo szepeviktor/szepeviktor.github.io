c1584c260771d5d781a37c07387e02a45bd3db8e
Viktor Szépe <viktor@szepe.net>
2014-11-28 21:54:32 +0100

    heading fixed

---
+++ add_heading.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sed -i 's|<body>|<head><title>Modern webserver solutions</title></head><body><h1>Modern webserver solutions</h1><h2>Debian Linux packages backported from jessie</h2>|' \
+    debian/index.html
