f70493c159a3db4605c5497651464423ac433a96
Viktor Szépe <viktor@szepe.net>
2014-12-04 04:24:59 +0100

    removed pcre, updated wget,silversearcher-ag

--- make-index.sh
+++ make-index.sh
@@ -7,9 +7,15 @@ echo "REMOVE:  reprepro remove wheezy <PKG>"
 echo "reprepro includedeb wheezy *.deb"
 
 
-which index_gen.py &> /dev/null || exit 1
+which index_gen.py &> /dev/null || exit 99
 
 pushd debian/
+
+# verify all packages
+find -type f -name "*.deb" \
+    | while read PKG; do
+        dpkg-sig --verify "$PKG" || exit 1
+    done
 
 index_gen.py > index.html
 sed -i 's|<body>|<head><title>Modern webserver solutions</title></head><body><h1>Modern webserver solutions</h1><h2>Debian Linux packages backported from jessie</h2>|' \
