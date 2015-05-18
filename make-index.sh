0c1c8b9da85c977cd4a4c0b127837b1c6c014d4f
Viktor Szépe <viktor@szepe.net>
2015-05-18 22:14:16 +0200

    php5-suhosin-extension and signatures

--- make-index.sh
+++ make-index.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
+#
+# Verify package signatures and generate index.
+#
+# VERSION       :0.2
+# DATE          :2015-05-18
+# AUTHOR        :Viktor Szépe <viktor@szepe.net>
+# LICENSE       :The MIT License (MIT)
+# URL           :https://github.com/szepeviktor/debian-server-tools
+# BASH-VERSION  :4.2+
+# DEPENDS       :apt-get install dpkg-sig
+# DEPENDS       :https://github.com/szepeviktor/debian-server-tools/blob/master/package/index_gen.py
 
 # my GPG key
 echo "dpkg-sig -k 451A4FBA --sign builder *.deb"
 echo "cd debian/"
 echo "REMOVE:  reprepro remove jessie <PKG>"
-echo "reprepro includedeb jessie *.deb"
+echo "reprepro includedeb jessie /var/cache/pbuilder/result/*.deb"
 
-
-# apt-get install dpkg-sig
-# index_gen.py: https://github.com/szepeviktor/debian-server-tools/tree/master/package
 which dpkg-sig index_gen.py &> /dev/null || exit 99
 
 pushd debian/
 
-# verify all packages
-find -type f -name "*.deb" \
-    | while read PKG; do
-        dpkg-sig --verify "$PKG" || exit 1
-    done
+# Verify all packages
+DEBS="$(find -type f -name "*.deb")"
+while read PKG; do
+    echo -n "${PKG} ... "
+    dpkg-sig --verify "$PKG" | grep "^GOODSIG" || exit 1
+done <<< "$DEBS"
 
+# Generate index
 index_gen.py > index.html
 sed -i 's|<body>|<head><title>Modern webserver solutions</title></head><body><h1>Modern webserver solutions</h1><h2>Debian Linux packages backported from jessie</h2>|' \
     index.html
@@ -28,5 +38,6 @@ popd
 echo "Index generated."
 
 git status
+
 echo
 echo "git add --all"
