a3e9935912601f2f8c2fb20093432fb364ee4a1e
Viktor Szépe <viktor@szepe.net>
2016-11-23 18:28:13 +0000

    Update make-index.sh

--- make-index.sh
+++ make-index.sh
@@ -2,44 +2,49 @@
 #
 # Verify package signatures and generate index.
 #
-# VERSION       :0.2.2
+# VERSION       :0.2.3
 # DATE          :2015-05-18
 # AUTHOR        :Viktor Szépe <viktor@szepe.net>
 # LICENSE       :The MIT License (MIT)
 # URL           :https://github.com/szepeviktor/debian-server-tools
 # BASH-VERSION  :4.2+
-# DEPENDS       :apt-get install dpkg-sig
+# DEPENDS       :apt-get install dpkg-sig reprepro
 # DEPENDS       :/usr/local/bin/index_gen.py
 
-cat <<EOF
-# My GPG key
-dpkg-sig -k 451A4FBA --sign builder /opt/results/*.deb
+set -e
+
+# Before this script
+cat <<"EOF"
+# Sign it with my GPG key
+dpkg-sig --sign "builder" -k "451A4FBA" /opt/results/*.deb
 cd debian/
 # REMOVE:  reprepro remove jessie <PKG>
 reprepro includedeb jessie /opt/results/*.deb
 EOF
 
-which index_gen.py &> /dev/null || exit 99
+(
+    cd debian/
 
-pushd debian/ || exit 1
+    # Verify packages updated in the last month
+    DEBS="$(find -type f -mtime -30 -name "*.deb")"
+    while read -r PKG; do
+        echo -n "${PKG} ... "
+        dpkg-sig --verify "$PKG" | grep --color "^GOODSIG"
+    done <<< "$DEBS"
 
-# Verify all packages
-DEBS="$(find -type f -mtime -30 -name "*.deb")"
-while read PKG; do
-    echo -n "${PKG} ... "
-    dpkg-sig --verify "$PKG" | grep --color "^GOODSIG" || exit 2
-done <<< "$DEBS"
-
-# Generate index
-index_gen.py > index.html
-sed -i -e 's|<body>|<head><title>Modern webserver solutions</title></head><body><h1>Modern webserver solutions</h1><h2>Freshly packaged and backported Debian Linux packages</h2>|' \
-    index.html
-
-popd
-
-echo "Index generated."
+    # Generate index
+    index_gen.py > index.html
+    sed -i -e 's|<body>|<head><title>Modern webserver solutions</title></head><body><h1>Modern webserver solutions</h1><h2>Freshly packaged and backported Debian Linux packages</h2>|' \
+        index.html
+    echo "Index generated."
+)
 
 git status -s
 
-echo
-echo "git add --all"
+# After this script
+cat <<"EOF"
+
+git add --all
+git commit
+git push
+EOF
